65d8c0244707148ce7822f9fb40679d59b029f5e
Arbaaz Khan <arbaazkhan2@gmail.com>
2017-04-12 01:52:30 -0400

    working versions

--- network.py
+++ network.py
@@ -32,8 +32,11 @@ class A2CContinuous(object):
 
     def choose_action(self, state):
         """Choose an action."""
-        return self.session.run([self.action], feed_dict={self.states: [state]})[0]
-
+        mu = self.session.run([self.action_mu], feed_dict={self.states: [state]})[0]
+        sigma = self.session.run([self.action_sigma], feed_dict={self.states: [state]})[0]
+        action_sess =  self.session.run([self.action], feed_dict={self.states: [state]})[0]
+        #return self.session.run([self.action], feed_dict={self.states: [state]})[0]
+        return mu,sigma,action_sess
 
     def build_networks(self):
         self.states = tf.placeholder(tf.float32, [None, 13], name="states")
@@ -43,29 +46,44 @@ class A2CContinuous(object):
 
         # Actor network
         with tf.variable_scope("actor"):
-            mu = tf.contrib.layers.fully_connected(
+            op1 = tf.contrib.layers.fully_connected(
                 inputs=tf.expand_dims(self.states, 0),
+                num_outputs=64,
+                activation_fn=tf.tanh,
+                weights_initializer=tf.constant_initializer(0.0))
+
+            mu = tf.contrib.layers.fully_connected(
+                inputs=tf.expand_dims(op1, 0),
                 num_outputs=1,
-                activation_fn=None,
+                activation_fn=tf.tanh,
                 weights_initializer=tf.constant_initializer(0.0))
             mu = tf.squeeze(mu, name="mu")
 
-            sigma = tf.contrib.layers.fully_connected(
+            op2 = tf.contrib.layers.fully_connected(
                 inputs=tf.expand_dims(self.states, 0),
+                num_outputs=64,
+                activation_fn=tf.tanh,
+                weights_initializer=tf.constant_initializer(0.0))
+
+            sigma = tf.contrib.layers.fully_connected(
+                inputs=tf.expand_dims(op2, 0),
                 num_outputs=1,
-                activation_fn=None,
+                activation_fn=tf.tanh,
                 weights_initializer=tf.constant_initializer(0.0))
             sigma = tf.squeeze(sigma)
             sigma = tf.add(tf.nn.softplus(sigma), 1e-5, name="sigma")
-
             self.normal_dist = tf.contrib.distributions.Normal(mu, sigma)
             self.action = self.normal_dist.sample(1)
+            self.action_mu = mu
+            self.action_sigma = sigma
+
             #self.action = tf.clip_by_value(self.action, self.action_space.low[0], self.action_space.high[0], name="action")
 
             # Loss and train op
             self.loss = -self.normal_dist.log_prob(tf.squeeze(self.actions_taken)) * (self.critic_rewards - self.critic_feedback)
             # Add cross entropy cost to encourage exploration
             self.loss -= 1e-1 * self.normal_dist.entropy()
+            #self.loss -=  self.normal_dist.entropy()
             self.summary_actor_loss = tf.reduce_mean(self.loss)
 
             self.optimizer = tf.train.AdamOptimizer(learning_rate=self.config["actor_learning_rate"])
@@ -83,10 +101,11 @@ class A2CContinuous(object):
                 weights_initializer=tf.random_normal_initializer(),
                 biases_initializer=tf.constant_initializer(0.0))
 
+
             self.critic_value = tf.contrib.layers.fully_connected(
                 inputs=critic_L1,
                 num_outputs=1,
-                activation_fn=None,
+                activation_fn=tf.tanh,
                 weights_initializer=tf.random_normal_initializer(),
                 biases_initializer=tf.constant_initializer(0.0))
 
@@ -95,56 +114,21 @@ class A2CContinuous(object):
             critic_optimizer = tf.train.AdamOptimizer(learning_rate=self.config["critic_learning_rate"])
             self.critic_train = critic_optimizer.minimize(critic_loss, global_step=tf.contrib.framework.get_global_step())
 
-    def learner(self,state,reward,  action):
-        returns = reward 
-        qw_new = self.get_critic_value(state)
-        all_state = state
-        all_action = action
+    def learner(self,state,reward,action):
+        returns = reward
+        state1 = np.reshape(state,(1,13))
+        qw_new = self.get_critic_value(state1)
+        #all_state = state
+        #all_action = action
         results = self.session.run([self.critic_train, self.actor_train], feed_dict = {
-            self.states: all_state,
+            self.states: state,
             self.critic_target:returns,
-            self.states: all_state,
-            self.actions_taken: all_action,
+            self.states: state1,
+            self.actions_taken: action,
             self.critic_feedback: qw_new,
             self.critic_rewards: returns,
             #self.rewards: np.mean(episode_rewards),
             #self.episode_lengths: np.mean(episode_lengths)
         })
 
-    '''
-    def learn(self):
-        """Run learning algorithm"""
-        #reporter = Reporter()
-        config = self.config
-        total_n_trajectories = 0
-        for iteration in range(config["n_iter"]):
-            # Collect trajectories until we get timesteps_per_batch total timesteps
-            trajectories = self.get_trajectories()
-            total_n_trajectories += len(trajectories)
-            all_action = np.concatenate([trajectory["action"] for trajectory in trajectories])
-            all_state = np.concatenate([trajectory["state"] for trajectory in trajectories])
-            # Compute discounted sums of rewards
-            returns = np.concatenate([discount_rewards(trajectory["reward"], config["gamma"]) for trajectory in trajectories])
-            qw_new = self.get_critic_value(all_state)
-
-            episode_rewards = np.array([trajectory["reward"].sum() for trajectory in trajectories])  # episode total rewards
-            episode_lengths = np.array([len(trajectory["reward"]) for trajectory in trajectories])  # episode lengths
-
-            results = self.session.run([self.summary_op, self.critic_train, self.actor_train], feed_dict={
-                self.states: all_state,
-                self.critic_target: returns,
-                self.states: all_state,
-                self.actions_taken: all_action,
-                self.critic_feedback: qw_new,
-                self.critic_rewards: returns,
-                self.rewards: np.mean(episode_rewards),
-                self.episode_lengths: np.mean(episode_lengths)
-            })
-            self.writer.add_summary(results[0], iteration)
-            self.writer.flush()
-
-            reporter.print_iteration_stats(iteration, episode_rewards, episode_lengths, total_n_trajectories)
-
-        if self.config["save_model"]:
-            self.saver.save(self.session, os.path.join(self.monitor_path, "model")) 
-        '''
+    
--- trainer.py
+++ trainer.py
@@ -24,68 +24,95 @@ mode_srv = rospy.ServiceProxy("/mavros/set_mode", SetMode)
 
 result = mode_srv(custom_mode="OFFBOARD")
 result = arming_srv(value=True)
-yaml_file = 'params.yaml'
 
-settings = {}
-stream = open(yaml_file, "r")
-docs = yaml.load_all(stream)
-for doc in docs:
-  for k,v in doc.items():
-    settings[k] = v
-pdb.set_trace()
 
-agent = A2CContinuous(settings)
-action_executed = TwistStamped()
-
-def trainer(vel, pos):
-    state = np.zeros((1,13))
-    print "this is good boy"
-    state[0,0]  = pos.pose.position.x
-    state[0,1]  = pos.pose.position.y
-    state[0,2]  = pos.pose.position.z
-    state[0,3]  = pos.pose.orientation.x
-    state[0,4]  = pos.pose.orientation.y
-    state[0,5]  = pos.pose.orientation.z
-    state[0,6]  = pos.pose.orientation.w
-    state[0,7]  = vel.twist.linear.x
-    state[0,8]  = vel.twist.linear.y
-    state[0,9]  = vel.twist.linear.z
-    state[0,10] = vel.twist.angular.x
-    state[0,11] = vel.twist.angular.y
-    state[0,12] = vel.twist.angular.z
-    pdb.set_trace()
-    print state
-    state = np.reshape(state,(13,))
-    pdb.set_trace()
-    action = agent.choose_action(state)[0]
-    pdb.set_trace()
-    action_executed.twist.linear.x =  action
-    vel_pub.publish(action_executed)
-    pdb.set_trace() 
+class reinforcement_hover:
     
-    #publish these states and observe the reward 
-    #TODO write code for publishing the state (start with vel x and vel y)
-    #TODO write code for computing reward (for now simple (x2-x1)^2 + (y2-y1)^2)
-    #the x2 and y2 are cordinates for the target x and target y. Since we want 
-    #maximization of reward, we put a negative sign in front of it 
-    agent.learner(reward, states, action_executed)
 
-    
-    #print vel
-    #print "-----------------------------"
-    #print pos
-    
-rospy.init_node('f450_velocity_controller', anonymous=True)
-rospy.set_param("/mavros/conn/heartbeat_rate", '3.0');
-vel_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped)
-vel_sub = message_filters.Subscriber('/mavros/local_position/velocity', TwistStamped)
-pos_sub = message_filters.Subscriber('/mavros/local_position/pose', PoseStamped)
-ts = message_filters.TimeSynchronizer([vel_sub, pos_sub], 10)
-ts.registerCallback(trainer) 
-attPub = rospy.Publisher('mavros/setpoint_attitude/attitude', PoseStamped, queue_size=10)
-arming_srv = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
-mode_srv = rospy.ServiceProxy("/mavros/set_mode", SetMode)
-v_p = rospy.Publisher('/mavros/setpoint_attitude/cmd_vel', TwistStamped, queue_size=10)
+    def __init__(self):
+        yaml_file = 'params.yaml'
+        settings = {}
+        stream = open(yaml_file, "r")
+        docs = yaml.load_all(stream)
+        for doc in docs:
+            for k,v in doc.items():
+                settings[k] = v
+        self.counter = 0
+        self.agent = A2CContinuous(settings)
+        self.action_executed = TwistStamped()
+        self.target = PoseStamped()
+        self.target.pose.position.x = 20
+        self.target.pose.position.y = 15
+        self.target.pose.position.z = 5
+
+        rospy.set_param("/mavros/conn/heartbeat_rate", '3.0');
+        self.vel_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped)
+        vel_sub = message_filters.Subscriber('/mavros/local_position/velocity', TwistStamped)
+        pos_sub = message_filters.Subscriber('/mavros/local_position/pose', PoseStamped)
+        ts = message_filters.TimeSynchronizer([vel_sub, pos_sub], 10)
+        ts.registerCallback(self.trainer) 
+        attPub = rospy.Publisher('mavros/setpoint_attitude/attitude', PoseStamped, queue_size=10)
+        arming_srv = rospy.ServiceProxy("/mavros/cmd/arming", CommandBool)
+        mode_srv = rospy.ServiceProxy("/mavros/set_mode", SetMode)
+        v_p = rospy.Publisher('/mavros/setpoint_attitude/cmd_vel', TwistStamped, queue_size=10)
+        self.action_executed = TwistStamped()
+        print "Setting Offboard Mode"
+        result = mode_srv(custom_mode="OFFBOARD")
+        print result
+        print "Arming"
+        result = arming_srv(value=True)
+
+    def trainer(self,vel, pos):
+        self.counter += 1
+        state = np.zeros((1,13))
+        print self.counter
+        state[0,0]  = pos.pose.position.x
+        state[0,1]  = pos.pose.position.y
+        state[0,2]  = pos.pose.position.z
+        state[0,3]  = pos.pose.orientation.x
+        state[0,4]  = pos.pose.orientation.y
+        state[0,5]  = pos.pose.orientation.z
+        state[0,6]  = pos.pose.orientation.w
+        state[0,7]  = vel.twist.linear.x
+        state[0,8]  = vel.twist.linear.y
+        state[0,9]  = vel.twist.linear.z
+        state[0,10] = vel.twist.angular.x
+        state[0,11] = vel.twist.angular.y
+        state[0,12] = vel.twist.angular.z
+        self.reward = self.calc_reward(state[0,0], state[0,1], self.target, state[0,7], state[0,8],state[0,9])
+        #self.old_pose.pose.position.x = state[0,0]
+        #self.old_pose.pose.position.y = state[0,1]
+
+        print self.reward
+        state = np.reshape(state,(13,))
+        
+        action = self.agent.choose_action(state)
+        #pdb.set_trace()
+
+        self.action_executed.twist.linear.x =  np.random.normal(loc = action[0],scale = action[1])
+        self.action_executed.twist.linear.y =  np.random.normal(loc = action[0],scale = action[1])
+        #self.action_executed.twist.linear.z =  np.random.normal(loc = action[0],scale = action[1])
+        #self.action_executed.twist.angular.x =  np.random.normal(loc = action[0],scale = action[1])
+        #self.action_executed.twist.angular.y =  np.random.normal(loc = action[0],scale = action[1])
+        #self.action_executed.twist.angular.z =  np.random.normal(loc = action[0],scale = action[1])
+
+        self.vel_pub.publish(self.action_executed)
+        self.agent.learner(state,self.reward, action[2])
+
+    def calc_reward(self,new_x,new_y,old_pose,velocity_x, velocity_y, velocity_z):
+            reward = np.square((new_x - old_pose.pose.position.x)) + np.square((new_y - old_pose.pose.position.y)) 
+            vel_reward = np.square(velocity_x) + np.square(velocity_y) + np.square(velocity_z)
+            total_reward = 0.6*reward + 0.4*vel_reward
+            return (-total_reward)
 
 
-rospy.spin()
+def main():
+        rospy.init_node('f450_velocity_controller', anonymous=True)
+        t_ = reinforcement_hover()
+        rospy.spin()
+
+
+if __name__=='__main__':
+        main()  
+
+
